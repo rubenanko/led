cce25f6b5bcd880af7b62c06e6826416bc1bfd62
rubenanko <rubenanko@gmail.com>
2025-09-18 11:58:44 +0200

    fix(): vertical line cleaning

--- ui.py
+++ ui.py
@@ -21,24 +21,29 @@ def fill_line(strlen:int,columns_number:int):
 def render(updateBuffer:bool = True):
     columns_number = get_terminal_size().columns
     lines_number = get_terminal_size().lines
+    displayed_lines_number = 1 + 1 + (3 + GLOBALS["NUMBER_OF_DIGITS"] + len(GLOBALS["LINE_BUFFER_LEFT"] + GLOBALS["LINE_BUFFER_RIGHT"])) // columns_number
     save_status_char = "\x1b[1;32m~\x1b[0;0m" if GLOBALS["IS_FILE_SAVED"] else "\x1b[1;35m~\x1b[0;0m"
     current_digit_number = get_max_digit_number(GLOBALS["LINE_INDEX"]+1)
-    cursor_coordinates = f'\x1b[{1 + (GLOBALS["COLUMN_INDEX"][GLOBALS["LINE_INDEX"]]) // columns_number};{1 + (GLOBALS["COLUMN_INDEX"][GLOBALS["LINE_INDEX"]] + GLOBALS["NUMBER_OF_DIGITS"] + 3) % columns_number}f'
+    cursor_coordinates = f'\x1b[{1 + (GLOBALS["COLUMN_INDEX"][GLOBALS["LINE_INDEX"]] + GLOBALS["NUMBER_OF_DIGITS"] + 3) // columns_number};{1 + (GLOBALS["COLUMN_INDEX"][GLOBALS["LINE_INDEX"]] + GLOBALS["NUMBER_OF_DIGITS"] + 3) % columns_number}f'
     highlighted_buffer = highlight(GLOBALS["LINE_BUFFER_LEFT"] + GLOBALS["LINE_BUFFER_RIGHT"],GLOBALS["LEXER"],TerminalFormatter())[:-1]
-
+    
+    #building the file display buffer
     inf = max(0,GLOBALS["LINE_INDEX"]-(lines_number//3))
-    sup = min(len(GLOBALS["BUFFER"]),GLOBALS["LINE_INDEX"]+(lines_number//3))
-    # top_file_buffer = "\n".join(GLOBALS["BUFFER"][inf:GLOBALS["LINE_INDEX"]])
-    # bottom_file_buffer = "\n".join(GLOBALS["BUFFER"][GLOBALS["LINE_INDEX"]+1:sup])
+    sup = min(len(GLOBALS["BUFFER"]),GLOBALS["LINE_INDEX"]+(lines_number//3) + max(0,-GLOBALS["LINE_INDEX"]+(lines_number//3)))
 
     top_file_buffer = ""
     for index in range(inf,GLOBALS["LINE_INDEX"]):
+        displayed_lines_number += 1 + (3 + GLOBALS["NUMBER_OF_DIGITS"] + len(GLOBALS["BUFFER"][index])) // columns_number
         top_file_buffer += f'\x1b[0;33m{index+1}\x1b[0;0m{" " * (GLOBALS["NUMBER_OF_DIGITS"] - get_max_digit_number(index+1))}\x1b[1;34m ~\x1b[0;0m {highlight(GLOBALS["BUFFER"][index],GLOBALS["LEXER"],TerminalFormatter())[:-1]}{fill_line(len(GLOBALS["BUFFER"][index])+ 3 + GLOBALS["NUMBER_OF_DIGITS"],columns_number)}\n'
             
     bottom_file_buffer = ""
     for index in range(GLOBALS["LINE_INDEX"]+1,sup):
+        displayed_lines_number += 1 + (3 + GLOBALS["NUMBER_OF_DIGITS"] + len(GLOBALS["BUFFER"][index])) // columns_number
         bottom_file_buffer += f'\x1b[0;33m{index+1}\x1b[0;0m{" " * (GLOBALS["NUMBER_OF_DIGITS"] - get_max_digit_number(index+1))}\x1b[1;34m ~\x1b[0;0m {highlight(GLOBALS["BUFFER"][index],GLOBALS["LEXER"],TerminalFormatter())[:-1]}{fill_line(len(GLOBALS["BUFFER"][index])+ 3 + GLOBALS["NUMBER_OF_DIGITS"],columns_number)}\n'
+
+    displayed_lines_number += 1 + (3 + GLOBALS["NUMBER_OF_DIGITS"] + len(GLOBALS["BUFFER"][GLOBALS["LINE_INDEX"]])) // columns_number
 
     file_highlighted_buffer = f'{top_file_buffer if len(top_file_buffer) > 0 else ""}\x1b[1;31m{GLOBALS["LINE_INDEX"]+1}{" " * (GLOBALS["NUMBER_OF_DIGITS"] - get_max_digit_number(GLOBALS["LINE_INDEX"]+1))} > \x1b[7;39m{GLOBALS["BUFFER"][GLOBALS["LINE_INDEX"]]}{fill_line(len(GLOBALS["BUFFER"][GLOBALS["LINE_INDEX"]])+ 3 + GLOBALS["NUMBER_OF_DIGITS"],columns_number)}\n{bottom_file_buffer}'
 
-    print(f'\x1b[1;1f\x1b[0;33m{GLOBALS["LINE_INDEX"]+1}\x1b[0;0m{" " * (GLOBALS["NUMBER_OF_DIGITS"] - current_digit_number)} {save_status_char} {highlighted_buffer}{" " * ((columns_number - 5 - len(GLOBALS["LINE_BUFFER_LEFT"] + GLOBALS["LINE_BUFFER_RIGHT"])) % columns_number)}\n{"-"*columns_number}\n{file_highlighted_buffer}{cursor_coordinates}',end="",flush=True)
+    #actual rendering
+    print(f'\x1b[1;1f\x1b[0;33m{GLOBALS["LINE_INDEX"]+1}\x1b[0;0m{" " * (GLOBALS["NUMBER_OF_DIGITS"] - current_digit_number)} {save_status_char} {highlighted_buffer}{" " * ((columns_number - 3 - GLOBALS["NUMBER_OF_DIGITS"] - len(GLOBALS["LINE_BUFFER_LEFT"] + GLOBALS["LINE_BUFFER_RIGHT"])) % columns_number)}\n{"-"*columns_number}\n{file_highlighted_buffer}{" "*(lines_number-displayed_lines_number)*columns_number}{cursor_coordinates}',end="",flush=True)
